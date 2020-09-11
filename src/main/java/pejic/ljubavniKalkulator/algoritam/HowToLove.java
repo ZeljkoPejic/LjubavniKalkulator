c355569ba091957213fd1336ac5f0719ded0070f
Zeljko Pejic <zeljkopejic@yahoo.com>
2020-09-11 13:21:16 +0200

    Kreirane klase za izradu ljubavnog kalkulatora

---
+++ src/main/java/pejic/ljubavniKalkulator/algoritam/HowToLove.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pejic.ljubavniKalkulator.algoritam;
+
+/**
+ *
+ * @author Pejić
+ */
+public class HowToLove {
+    
+    
+    public static int checkLove(String prvoIme, String drugoIme){
+        
+        
+        
+        
+        return 0;
+        
+    }
+        
+    
+    
+    
+}
